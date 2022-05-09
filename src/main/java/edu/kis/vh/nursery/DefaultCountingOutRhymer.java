320287ed7ba71a0bf31103031473dea0b57e2e2c
Wiluś Osada <235940@edu.p.lodz.pl>
2022-05-09 10:49:31 +0200

    9 - Dodanie gettera dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,6 +6,10 @@ public class DefaultCountingOutRhymer {
     private static final int ERROR = -1;
     private final int[] numbers = new int[amountOfNumbers];
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = ERROR;
 
     public void countIn(int in) {
